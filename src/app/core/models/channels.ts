6996b1f996291c0a60ad7fd6b5e99a0fe071bfe7
Michal Halabica <m.halabica@gmail.com>
2021-10-06 23:29:58 +0200

    Implemented service and models for channels.

--- src/app/core/models/channels.ts
+++ src/app/core/models/channels.ts
@@ -1,5 +1,8 @@
 import { DateTime } from './datetime';
 import { ChannelType } from './enums/channel-type';
+import { Guild } from './guilds';
+import { QueryParam } from './http';
+import { User } from './users';
 
 export class Channel {
     public id: string;
@@ -33,6 +36,126 @@ export class ChannelStatItem {
         item.count = data.count;
         item.firstMessageAt = DateTime.fromISOString(data.firstMessageAt);
         item.lastMessageAt = DateTime.fromISOString(data.lastMessageAt);
+
+        return item;
+    }
+}
+
+export class SendMessageToChannelParams {
+    public content: string;
+    public reference: string | null;
+
+    static create(form: any): SendMessageToChannelParams | null {
+        if (!form) { return null; }
+        const params = new SendMessageToChannelParams();
+
+        params.content = form.content;
+        params.reference = form.reference;
+
+        return params;
+    }
+}
+
+export class GetChannelListParams {
+    public guildId: string | null;
+    public nameContains: string | null;
+    public channelType: ChannelType | null;
+
+    get queryParams(): QueryParam[] {
+        return [
+            this.guildId ? new QueryParam('guildId', this.guildId) : null,
+            this.nameContains ? new QueryParam('nameContains', this.nameContains) : null,
+            this.channelType ? new QueryParam('channelType', this.channelType) : null
+        ].filter(o => o);
+    }
+
+    static get empty(): GetChannelListParams { return new GetChannelListParams(); }
+
+    static create(form: any): GetChannelListParams | null {
+        if (!form) { return null; }
+        const params = new GetChannelListParams();
+
+        params.channelType = form.channelType;
+        params.guildId = form.guildId;
+        params.nameContains = form.nameContains;
+
+        return params;
+    }
+}
+
+export type ChannelListSortTypes = 'name' | 'type';
+
+export class GuildChannel {
+    public id: string;
+    public name: string;
+    public type: ChannelType;
+    public guild: Guild;
+
+    static create(data: any): GuildChannel | null {
+        if (!data) { return null; }
+        const channel = new GuildChannel();
+
+        channel.id = data.id;
+        channel.guild = data.guild ? Guild.create(data.guild) : null;
+        channel.name = data.name;
+        channel.type = data.type;
+
+        return channel;
+    }
+}
+
+export class ChannelDetail extends GuildChannel {
+    public flags: number;
+    public firstMessageAt: DateTime | null;
+    public lastMessageAt: DateTime | null;
+    public lastMessageFrom: User | null;
+    public mostActiveUser: User | null;
+
+    static create(data: any): ChannelDetail | null {
+        if (!data) { return null; }
+        const base = super.create(data);
+        const detail = new ChannelDetail();
+
+        detail.firstMessageAt = data.firstMessageAt ? DateTime.fromISOString(data.firstMessageAt) : null;
+        detail.flags = data.flags;
+        detail.guild = base.guild;
+        detail.id = base.id;
+        detail.lastMessageAt = data.lastMessageAt ? DateTime.fromISOString(data.lastMessageAt) : null;
+        detail.lastMessageFrom = data.lastMessageFrom ? User.create(data.lastMessageFrom) : null;
+        detail.mostActiveUser = data.mostActiveUser ? User.create(data.mostActiveUser) : null;
+        detail.name = data.name;
+        detail.type = data.type;
+
+        return detail;
+    }
+}
+
+export class UpdateChannelParams {
+    constructor(
+        public flags: number
+    ) { }
+}
+
+export class ChannelUserStatItem {
+    public position: number;
+    public username: string;
+    public nickname: string | null;
+    public userId: string;
+    public count: number;
+    public firstMessageAt: DateTime;
+    public lastMessageAt: DateTime;
+
+    static create(data: any): ChannelUserStatItem | null {
+        if (!data) { return null; }
+        const item = new ChannelUserStatItem();
+
+        item.count = data.count;
+        item.firstMessageAt = DateTime.fromISOString(data.firstMessageAt);
+        item.lastMessageAt = DateTime.fromISOString(data.lastMessageAt);
+        item.nickname = data.nickname;
+        item.position = data.position;
+        item.userId = data.userId;
+        item.username = data.username;
 
         return item;
     }
---
+++ src/app/core/services/channel.service.ts
@@ -0,0 +1,83 @@
+import { PaginatedResponse } from './../models/common';
+import { QueryParam } from './../models/http';
+import { catchError, map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { BaseService } from './base.service';
+import { Injectable } from '@angular/core';
+import {
+    ChannelDetail, ChannelListSortTypes, ChannelUserStatItem, GetChannelListParams,
+    GuildChannel, SendMessageToChannelParams, UpdateChannelParams
+} from '../models/channels';
+import { environment } from 'src/environments/environment';
+import { PaginatedParams } from '../models/common';
+
+@Injectable({ providedIn: 'root' })
+export class ChannelService {
+    constructor(
+        private base: BaseService
+    ) { }
+
+    sendMessageToChannel(guildId: string, channelId: string, params: SendMessageToChannelParams): Observable<unknown> {
+        const url = `${environment.apiUrl}/channel/${guildId}/${channelId}`;
+        const headers = this.base.getHttpHeaders();
+
+        return this.base.http.post(url, params, { headers }).pipe(
+            catchError(err => this.base.catchError(err))
+        );
+    }
+
+    getChannelsList(filter: GetChannelListParams, pagination: PaginatedParams, sortBy: ChannelListSortTypes, sortDesc: boolean):
+        Observable<PaginatedResponse<GuildChannel>> {
+        const parameters = [
+            ...filter.queryParams,
+            ...pagination.queryParams,
+            new QueryParam('sortBy', sortBy),
+            new QueryParam('sortDesc', sortDesc)
+        ].filter(o => o).map(o => o.toString()).join('&');
+        const url = `${environment.apiUrl}/channel?${parameters}`;
+        const headers = this.base.getHttpHeaders();
+
+        return this.base.http.get<PaginatedResponse<GuildChannel>>(url, { headers }).pipe(
+            map(data => PaginatedResponse.create(data, entity => GuildChannel.create(entity))),
+            catchError(err => this.base.catchError(err))
+        );
+    }
+
+    removeMessagesFromCache(guildId: string, channelId: string): Observable<unknown> {
+        const url = `${environment.apiUrl}/channel/${guildId}/${channelId}/cache`;
+        const headers = this.base.getHttpHeaders();
+
+        return this.base.http.delete(url, { headers }).pipe(
+            catchError(err => this.base.catchError(err))
+        );
+    }
+
+    getChannelDetail(id: number): Observable<ChannelDetail> {
+        const url = `${environment.apiUrl}/channel/${id}`;
+        const headers = this.base.getHttpHeaders();
+
+        return this.base.http.get<ChannelDetail>(url, { headers }).pipe(
+            map(data => ChannelDetail.create(data)),
+            catchError(err => this.base.catchError(err))
+        );
+    }
+
+    updateChannel(id: number, params: UpdateChannelParams): Observable<unknown> {
+        const url = `${environment.apiUrl}/channel/${id}`;
+        const headers = this.base.getHttpHeaders();
+
+        return this.base.http.put(url, params, { headers }).pipe(
+            catchError(err => this.base.catchError(err))
+        );
+    }
+
+    getUserStatsOfChannel(id: number): Observable<PaginatedResponse<ChannelUserStatItem>> {
+        const url = `${environment.apiUrl}/channel/${id}/userStats`;
+        const headers = this.base.getHttpHeaders();
+
+        return this.base.http.get<PaginatedResponse<ChannelUserStatItem>>(url, { headers }).pipe(
+            map(data => PaginatedResponse.create(data, entity => ChannelUserStatItem.create(entity))),
+            catchError(err => this.base.catchError(err))
+        );
+    }
+}
