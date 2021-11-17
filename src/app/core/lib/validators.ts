19d843a47376b391cf59dd3af4e7227ffd9e6eee
Michal Halabica <m.halabica@gmail.com>
2021-11-17 20:28:16 +0100

    Removed unused method

--- src/app/core/lib/validators.ts
+++ src/app/core/lib/validators.ts
@@ -1,14 +1,6 @@
-import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
-import * as Guid from 'guid';
+import { AbstractControl } from '@angular/forms';
 
 export class ValidationHelper {
-    static isGuid(): ValidatorFn {
-        return (control: AbstractControl): ValidationErrors => {
-            const value: string = control.value;
-            return value && Guid.isGuid(value) ? null : { guid: true };
-        };
-    }
-
     static isInvalid(form: AbstractControl, controlId: string, errorId: string = null): boolean {
         const control = form.get(controlId);
         if (!control.touched) { return false; }
